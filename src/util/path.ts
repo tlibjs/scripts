b0d19eb84fd75c70f23eb6b8714621660b9da018
EqualMa <equalma@outlook.com>
2021-10-31 11:26:27 +0000

    feat: util path deepTrimStartDir

--- src/util/path.ts
+++ src/util/path.ts
@@ -1,4 +1,5 @@
 import { posix as pp } from "path";
+import { deepMap } from "./deep-map";
 
 export function joinPath(...paths: string[]): string {
   return pp.join(...paths);
@@ -9,4 +10,30 @@ export function trimStartDir(p: string, startDir: string): string {
 
   if (res.startsWith("../")) return p;
   else return res || ".";
+}
+
+export type NestedPathValue =
+  | string
+  | NestedPathValue[]
+  | {
+      [K in PropertyKey]: unknown;
+    }
+  | undefined
+  | null
+  | number
+  | boolean
+  | bigint
+  | symbol
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  | ((...args: any[]) => any);
+
+export function deepTrimStartDir<T extends NestedPathValue>(
+  p: T,
+  startDir: string,
+): T {
+  return deepMap<T, unknown>(
+    p,
+    (v: unknown) => (typeof v === "string" ? trimStartDir(v, startDir) : v),
+    { cacheForTypes: ["string", "null"] },
+  ) as never;
 }
