707f47591413ae9fbfa0a70be0f504cf1e115804
EqualMa <equalma@outlook.com>
2021-10-31 08:49:00 +0000

    feat: util/path trimStartDir

--- src/util/path.ts
+++ src/util/path.ts
@@ -3,3 +3,10 @@ import { posix as pp } from "path";
 export function joinPath(...paths: string[]): string {
   return pp.join(...paths);
 }
+
+export function trimStartDir(p: string, startDir: string): string {
+  const res = pp.relative(startDir, p);
+
+  if (res.startsWith("../")) return p;
+  else return res || ".";
+}
