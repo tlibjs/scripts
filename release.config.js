1729dbb8940a7885ef365f3c1ea45d603c4209f7
EqualMa <equalma@outlook.com>
2021-11-01 21:08:11 +0000

    chore: copy meta files with rollup instead of semantic-release
    Fixes #1

--- release.config.js
+++ release.config.js
@@ -14,11 +14,7 @@ module.exports = {
     ["@semantic-release/npm", { pkgRoot: "dist" }],
     [
       "@semantic-release/exec",
-      {
-        // https://semantic-release.gitbook.io/semantic-release/support/faq#how-can-i-use-a-npm-build-script-that-requires-the-package-jsons-version
-        prepareCmd:
-          "node scripts/prepublish.js && node scripts/postversion.js -v ${nextRelease.version}",
-      },
+      { prepareCmd: "node scripts/postversion.js -v ${nextRelease.version}" },
     ],
     "@semantic-release/git",
     "@semantic-release/github",
