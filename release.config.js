da7ae1cb5537e2047dfa84ffc20f8427d420ae3e
EqualMa <equalma@outlook.com>
2021-11-01 20:42:15 +0000

    chore: build and test before releasing

--- release.config.js
+++ release.config.js
@@ -12,14 +12,6 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     ["@semantic-release/npm", { pkgRoot: "dist" }],
-    [
-      "@semantic-release/exec",
-      {
-        // https://semantic-release.gitbook.io/semantic-release/support/faq#how-can-i-use-a-npm-build-script-that-requires-the-package-jsons-version
-        prepareCmd:
-          "node scripts/prepublish.js && node scripts/postversion.js -v ${nextRelease.version}",
-      },
-    ],
     "@semantic-release/git",
     "@semantic-release/github",
   ],
